af7f3cf7af14aba7698526e9ec637f28c1bc39db
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2024-10-26 18:16:19 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+import jsonfile from "jsonfile";
+import moment from "moment";
+import simpleGit from "simple-git";
+import random from "random";
+
+const path = "./data.json";
+const git = simpleGit();
+
+const makeCommits = async (n) => {
+    if (n === 0) {
+        await git.push();
+        return;
+    }
+
+    const x = random.int(0, 54);
+    const y = random.int(0, 6);
+    const date = moment().subtract(1, "year").add(1, "day").add(x, "weeks").add(y, "days").toISOString();
+
+    const data = { date };
+    console.log(date);
+
+    try {
+        await jsonfile.writeFile(path, data);
+        await git.add(path);
+        await git.commit(date, { '--date': date });
+        await makeCommits(n - 1);
+    } catch (error) {
+        console.error("Error during commit process:", error);
+    }
+};
+
+// Usage example
+makeCommits(5);
